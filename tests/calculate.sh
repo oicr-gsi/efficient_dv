92c7221f195f727f8c7c12badf2a26f2d0d2fca9
pruzanov <pruzanov@oicr.on.ca>
2026-05-28 14:08:25 -0400

    Added condition to calculate script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,4 +6,4 @@ set -o pipefail
 cd $1
 
 echo "Count of PASS variants from the output vcf"
-for v in *.vcf.gz;do zcat $v | grep -v ^# | awk '$7=="PASS" && $6>10' | wc -l | awk '{printf "%.0f\n", $0/1000}';done
+for v in *.vcf.gz;do zcat $v | grep -v ^# | awk '$7=="PASS" && $6>10' | wc -l | awk '{if $0 > 1000(printf "%.0f\n", $0/1000)}';done
